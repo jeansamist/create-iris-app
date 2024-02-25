da23547ebb05cccb16b32af541f1047c97bb6f5f
BAHA Ephraim <jeansamist@gmail.com>
2024-02-25 06:33:02 +0100

    testing cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
 /**
  *
  * @param {String} command
@@ -16,13 +19,28 @@ const runCommand = (command) => {
   return true;
 };
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/jeansamist/create-iris-app ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const pattern =
+  /^(?:(?:@(?:[a-z0-9-*~][a-z0-9-*._~]*)?\/[a-z0-9-._~])|[a-z0-9-~])[a-z0-9-._~]*$/;
 
-console.log("Clonning the repository");
-const checkout = runCommand(gitCheckoutCommand);
-if (!checkout) process.exit(-1);
-console.log("Installing dependencies");
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
-console.log("Project is ready");
+if (pattern.test(repoName)) {
+  const gitCheckoutCommand = `git clone --depth 1 https://github.com/jeansamist/create-iris-app ${repoName}`;
+  const installDepsCommand = `cd ${repoName} && npm install`;
+
+  console.log("Clonning the repository");
+  const checkout = runCommand(gitCheckoutCommand);
+  if (!checkout) process.exit(-1);
+
+  const pkg = path.join(path.join(__dirname, ".."), "package.json");
+  const json = JSON.parse(fs.readFileSync(pkg, "utf8"));
+  json["name"] = repoName;
+  json["version"] = "0.0.1";
+  fs.writeFileSync(pkg, JSON.stringify(json, null, 2), "utf8");
+
+  console.log("Installing dependencies");
+  const installedDeps = runCommand(installDepsCommand);
+  if (!installedDeps) process.exit(-1);
+
+  console.log("Project is ready");
+} else {
+  console.log("Text does not match the pattern");
+}
